298ec782fe52c03c4f342b6c1cf71ac176ea8721
Michael Travers <michael.travers@udc.edu>
2022-09-14 22:11:27 -0400

    working on shipping page

--- src/shoppingcarpages.js/paymentoptions.js
+++ src/shoppingcarpages.js/paymentoptions.js
@@ -1,24 +1,53 @@
 import React from "react";
+import Nextbuttom from "../components/nextbutton";
+import Summary from "../components/summary";
 
 const Paymentoption = () => {
     return(
         <div>
 
         <div id="paymentoptions">
-
+        <div>
         <form>
             <span>
-            <h1>Payment</h1>
+            <h1>Payment method</h1>
             <hr/>
-            <input></input>
-            <input></input>
+
+            <div id="creditcard">
+                <span>
+                    <div>
+                    <input type="radio" style={{float:"left"}}/><span><p>Credit card <br/> Lorem ipsum dolor sit</p> <p></p></span>
+                    </div>
+                </span>
+            <input id="smallbox"></input>
+            <input id="exsmallbox"></input>
+            <input id="exsmallbox"></input>
+            <input id="bigbox"></input>
+
+            </div>
+
+
             </span>
 
         </form>
+        <hr/>
+        <div id="paypal">
+            
+            
+            <span><p> <input style={{float:"left"}} type="radio"/><span style={{textAlign:"left"}}>Paypal</span><br/>pay with paypal<img style={{float:"right", width:"20px", height:"20px"}}/></p></span>
+            
+        </div>
+        
+        <div id="next"><hr style={{color:"black"}}/>
+                <button style={{}}>Pay Now</button>
+                <span style={{paddingLeft:"30px"}}>
+                <button >back</button>
+                </span>
+        </div>
+        </div>
 
         <div>
-            <h1>Summary</h1>
-            <hr/>
+            <Summary/>
         </div>
 
         </div>
