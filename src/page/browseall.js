1c115cdee0369a8357f3f5dfd23fc70fbbdbfe19
Michael Travers <michael.travers@udc.edu>
2022-09-15 23:55:58 -0400

    adding json server for collection

--- src/page/browseall.js
+++ src/page/browseall.js
@@ -2,30 +2,33 @@ import React from "react";
 import { useState } from "react";
 import Navbar from "../components/navbar";
 
-const Browseall = (props)=>{
+const Browseall = ({dress, handleDelete})=>{
     
-    const [dress, setDress]=useState([{productname:"dress1", rating:"4", price:"$400", id:1},
-                                    {productname:"dress2", rating:"4", price:"$500", id:2},
-                                   {productname:"dress3", rating:"5", price:"$600", id:3},
-                                   {productname:"dress4", rating:"5", price:"$700", id:4},
-                                   {productname:"dress5", rating:"5", price:"$800", id:5},
-                                   {productname:"dress6", rating:"5", price:"$900", id:6}])
+   
+    
+    // const [dress, useDress]=useState([{productname:"dress1", rating:"4", price:"$400", id:1},
+    //                                 {productname:"dress2", rating:"4", price:"$500", id:2},
+    //                                {productname:"dress3", rating:"5", price:"$600", id:3},
+    //                                {productname:"dress4", rating:"5", price:"$700", id:4},
+    //                                {productname:"dress5", rating:"5", price:"$800", id:5},
+    //                                {productname:"dress6", rating:"5", price:"$900", id:6}])
+
+
     return(
         <div >
-            <Navbar/>
+            
             <div id="dres">
-            {dress.map((dres)=>(
+            
+             {dress.map((dres)=>(
                 <div key={dres.id}>
-                   <span >
                     <h1>{dres.productname}</h1>
-                    {dres.rating}
                     {dres.price}
-                    </span>
-                
-                    
+                    {dres.rating}
+                    <button onClick={()=>handleDelete(dres.id)}>Delete</button>
                 </div>
-            ))}
-
+             )  )}
+                    
+                
 
                
             
--- src/page/collection.js
+++ src/page/collection.js
@@ -5,12 +5,26 @@ import Navbar from "../components/navbar";
 import ShopAll from "./shopAll";
 
 const Collection = ()=>{
-    const [dress, setDress]=useState([{productname:"dress1", rating:"4", price:"$400", id:1},
-                                    {productname:"dress2", rating:"4", price:"$500", id:2},
-                                   {productname:"dress3", rating:"5", price:"$600", id:3}])
+    const [dress, setDress]=useState([{productname:"dress1", rating:"2", price:"$400", id:1},
+                                    {productname:"dress2best", rating:"4", price:"$500", id:2},
+                                   {productname:"dress3", rating:"3", price:"$600", id:3},
+                                   {productname:"dress4", rating:"1", price:"$600", id:4},
+                                   {productname:"dress6best", rating:"5", price:"$600", id:5}])
+
+    const handleDelete=(id)=>{
+        const newdresslist = dress.filter(dres=>dres.id !==id)
+        setDress(newdresslist)
+    }
+
+    
+                
+
     return(
         <div id="collection">
-            <Browseall dress={dress}/>
+            <Browseall dress={dress} handleDelete={handleDelete} />
+            <div><p>best selling</p>
+            <Browseall dress={dress.filter((dres)=>dres.rating>=4)} />
+            </div>
             
             
             
