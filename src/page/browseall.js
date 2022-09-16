055ec48c20dc0d8a49d83956bf52913dd4b1ef4b
Michael Travers <michael.travers@udc.edu>
2022-09-16 11:32:36 -0400

    added a fetch request from the json server for dresses

--- src/page/browseall.js
+++ src/page/browseall.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { useState } from "react";
 import Navbar from "../components/navbar";
+import Collection from "./collection";
 
-const Browseall = ({dress, handleDelete})=>{
+
+const Browseall = ({dress})=>{
+    
     
    
     
-    // const [dress, useDress]=useState([{productname:"dress1", rating:"4", price:"$400", id:1},
+    //  const [dress, useDress]=useState([{productname:"dress1", rating:"4", price:"$400", id:1},
     //                                 {productname:"dress2", rating:"4", price:"$500", id:2},
     //                                {productname:"dress3", rating:"5", price:"$600", id:3},
-    //                                {productname:"dress4", rating:"5", price:"$700", id:4},
+    //                                 {productname:"dress4", rating:"5", price:"$700", id:4},
     //                                {productname:"dress5", rating:"5", price:"$800", id:5},
-    //                                {productname:"dress6", rating:"5", price:"$900", id:6}])
+    //                                 {productname:"dress6", rating:"5", price:"$900", id:6}])
 
 
     return(
@@ -24,7 +27,8 @@ const Browseall = ({dress, handleDelete})=>{
                     <h1>{dres.productname}</h1>
                     {dres.price}
                     {dres.rating}
-                    <button onClick={()=>handleDelete(dres.id)}>Delete</button>
+                
+                    {/* <button onClick={()=>handleDelete(dres.id)}>Delete</button> */}
                 </div>
              )  )}
                     
--- src/page/collection.js
+++ src/page/collection.js
@@ -1,29 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Browseall from "./browseall";
 import Navbar from "../components/navbar";
 import ShopAll from "./shopAll";
 
 const Collection = ()=>{
-    const [dress, setDress]=useState([{productname:"dress1", rating:"2", price:"$400", id:1},
-                                    {productname:"dress2best", rating:"4", price:"$500", id:2},
-                                   {productname:"dress3", rating:"3", price:"$600", id:3},
-                                   {productname:"dress4", rating:"1", price:"$600", id:4},
-                                   {productname:"dress6best", rating:"5", price:"$600", id:5}])
-
-    const handleDelete=(id)=>{
-        const newdresslist = dress.filter(dres=>dres.id !==id)
-        setDress(newdresslist)
-    }
-
+    const [dress, setDress]=useState(null)
     
+// const [dress, setDress]=useState([{productname:"dress1", rating:"2", price:"$400", id:1},
+//                                    {productname:"dress2best", rating:"4", price:"$500", id:2},
+//                                   {productname:"dress3", rating:"3", price:"$600", id:3},
+//                                   {productname:"dress4", rating:"1", price:"$600", id:4},
+//                                   {productname:"dress6best", rating:"5", price:"$600", id:5}])
+   
+
+    // const handleDelete=(id)=>{
+    //     const newdresslist = dress.filter(dres=>dres.id !==id)
+    //     setDress(newdresslist)
+    // }
+        useEffect(()=>{
+            fetch("http://localhost:8000/dress")
+            .then(res=>{
+                return res.json()
+            }).then(data=>{console.log(data)
+            setDress(data)})
+
+        }, [])
+    
+
                 
 
     return(
         <div id="collection">
-            <Browseall dress={dress} handleDelete={handleDelete} />
+            {dress &&<Browseall dress={dress}  />}
+            {/* <Browseall dress={dress} handleDelete={handleDelete} /> */}
             <div><p>best selling</p>
-            <Browseall dress={dress.filter((dres)=>dres.rating>=4)} />
+            {dress && <Browseall dress={dress.filter((dres)=>dres.rating>=4)} />}
             </div>
             
             
