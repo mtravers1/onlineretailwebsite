0a981132502ebbe0d1a8a3029edfc80745bb2266
Michael Travers <michael.travers@udc.edu>
2022-09-19 14:06:27 -0400

    working on lyalty page

--- src/page/loyaltycard.js
+++ src/page/loyaltycard.js
@@ -4,34 +4,102 @@ const Loyalty = ()=>{
     return(
 
         
-        <div id="loyaltycard">
+        <div  style={{backgroundColor:"#f6a2b4"}}>
                     <Navbar/>
 
-            <div>
-                <div id="loyalcn">
-                    <label>Loyal Card Number</label>
 
+                    <div>
+                        <form></form>
+                        <h1>Loyalty Enrollment form</h1>
+                        <p>All information will be kept confidential and not shared with anyone</p>
+                    </div>
+                <div id="body">
+            <div id="loyal">
+                <div>
+                <div id="loyalname" style={{height:"60px", alignItems:"center", justifyContent:"center"}} >
+                    
+                    <label for="cardnumber">Loyal Card Number</label>
+                    <input type='text' placeholder="0000"></input>
 
+                    </div>
                 </div>
 
-                <div id="loyalname">
-                    <label>Name</label>
+                
+                <div id="loyalname" style={{marginBottom:"50px"}}>
+                <h1>Name</h1>
+                <div id="center">
+                    <span><label>First Name</label>
+                    <input type="text"></input></span>
+                    
+                    <span><label>Last Name</label>
+                    <input type="text"></input></span>
 
+                    </div>
                 </div>
 
-                <div id="loyaladdress">
-                <label>Address</label>
+                <div>
 
+
+                <div id="loyalname" style={{marginBottom:"50px"}}>
+                <h1>Address</h1>
+                <div id="center">
+
+                    <span><label for="address">Line 1</label>
+                    <input type="text" name="Line1" ></input></span>
+                    
+                    <span><label for="address">Line 2</label>
+                    <input type="text" ></input></span>
+
+                   <span> <label for="address">Line 2</label>
+                    <input type="text" ></input></span>
+                    </div>
+
+
+                    </div>
                 </div>
 
-                <div id="loyalcontact">
-                <label>Contact info</label>
 
+
+
+
+
+
+                
+                <div id="loyalname" style={{marginBottom:"50px"}}>
+                
+                <h1>Contact Information</h1>
+                <div id="center">
+                    <span><label>phone</label>
+                    <input type="text"></input></span>
+                    
+                    <span><label>E-mail</label>
+                    <input type="text"></input></span>
+
+                    <span><label>Confirm E-mail</label>
+                    <input type="text"></input></span>
+                    
+                    </div>
                 </div>
 
-                <div id="loyalopt">
-                <label>Optional Information</label>
 
+
+               
+                <div id="loyalname" style={{marginBottom:"50px"}}>
+                
+                <h1>Optional</h1>
+                <div id="center">
+                    <span><label>Gender</label>
+                    <input type="text"></input></span>
+                    
+                    <span><label>Birthday</label>
+                    <input type="text"></input></span>
+
+                    </div>
+                </div>
+
+
+                
+                
                 </div>
 
 
@@ -40,6 +108,7 @@ const Loyalty = ()=>{
 
 
             </div>
+            <button type="Submit">Submit</button>
         </div>
     )
 }
